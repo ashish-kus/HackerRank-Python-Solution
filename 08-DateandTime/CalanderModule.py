b4e7582a20dd825d51bf06a49a38051813c7e541
ashish-kus <ashish.kus2408@gmail.com>
2024-01-11 23:32:42 +0530

    chasing target

---
+++ 08-DateandTime/CalanderModule.py
@@ -0,0 +1,6 @@
+import calendar 
+days=["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+month, day, year = map(int, input().split())
+print(days[calendar.weekday(year,month,day)])
+
+# print(*(map(int, input().split())))
